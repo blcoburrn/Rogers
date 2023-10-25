584f6df6f850e647dfb66c31acb60a5988b5d0d7
blcoburrn <149008646+blcoburrn@users.noreply.github.com>
2023-10-25 13:40:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_i2l5adc", "template_tpbt5s6", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
